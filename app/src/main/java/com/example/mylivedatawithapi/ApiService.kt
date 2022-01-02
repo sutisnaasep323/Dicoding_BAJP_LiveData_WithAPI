2ccd7f296b1efab455d377a24fb91fcafb680db3
Asep Sutisna <sutisnaasep323@gmail.com>
2022-01-02 21:35:56 +0700

    LiveData and Mutable LiveData

--- app/src/main/java/com/example/mylivedatawithapi/ApiService.kt
+++ app/src/main/java/com/example/mylivedatawithapi/ApiService.kt
@@ -1,13 +1,21 @@
 package com.example.mylivedatawithapi
 
 import retrofit2.Call
-import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.*
 
 interface ApiService {
     @GET("detail/{id}")
     fun getRestaurant(
         @Path("id") id: String
     ): Call<RestaurantResponse>
+
+    @FormUrlEncoded
+    @Headers("Authorization: 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")
+    @POST("review")
+    fun postReview(
+        @Field("id") id: String,
+        @Field("name") name: String,
+        @Field("review") review: String
+    ): Call<PostReviewResponse>
 
 }
